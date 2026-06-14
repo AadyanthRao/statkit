522ab36e10380842547182b4358c1a73eae46449
= <=>
2026-06-14 13:25:06 -0500

    Add stdev operation

--- statkit.py
+++ statkit.py
@@ -1,15 +1,22 @@
 import sys
+import math
 
-OPERATIONS = ["mean"]
+OPERATIONS = ["mean", "stdev"]
 
 def mean(values):
     return sum(values) / len(values)
+
+def stdev(values):
+    m = mean(values)
+    return math.sqrt(sum((x - m) ** 2 for x in values) / len(values))
 
 def main():
     op = sys.argv[1]
     nums = [float(x) for x in sys.argv[2:]]
     if op == "mean":
         print(mean(nums))
+    elif op == "stdev":
+        print(stdev(nums))
     else:
         print(f"Unknown operation '{op}'. Available: {OPERATIONS}")
 
